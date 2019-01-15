253353c2ef9a500ffb65874b4c487be502f7e4b5
cocosh03 <corentin.lexa@gmail.com>
2019-01-15 19:03:52 +0100

    v2

--- src/main.cpp
+++ src/main.cpp
@@ -56,56 +56,71 @@ std::vector<std::string> split(const std::string &chaine, char delimiteur)
 int main(int argc, char** argv) {
     int width = 800;
     int height = 800;
-    TGAImage image(width, height, TGAImage::RGB);
+    TGAImage image(900, 900, TGAImage::RGB);
 
     std::ifstream fichier("../obj/african_head.obj");
-    if (fichier){
+    if (fichier) {
         char delimiter = ' ';
         std::string ligne;
-        std::vector<std::string> tableau;
+        std::vector<std::string> vertices;
+        std::vector<std::string> edges;
 
-        while (getline(fichier,ligne)){
+
+        while (getline(fichier, ligne)) {
             std::vector<std::string> listLignes = split(ligne, delimiter);
-            if (listLignes.size() == 4){
-                if (listLignes[0] == "v"){
-                    tableau.push_back(listLignes[1]+ ' ' + listLignes[2]);
+            if (listLignes.size() == 4) {
+                if (listLignes[0] == "v") {
+                    vertices.push_back(listLignes[1] + ' ' + listLignes[2]);
+                }
+                if (listLignes[0] == "f") {
+                    char limiter = '/';
+                    std::string coordonees;
+                    for (int i = 1; i < 4; i++) {
+                        std::vector<std::string> temp = split(listLignes[i], limiter);
+                        coordonees += temp[0] + ' ';
+                    }
+                    edges.push_back(coordonees);
                 }
             }
 
         }
         fichier.close();
-        //image.set(float x0 = (v0.x+1.)*width/2.), flaot y0 = (v0.y+1.)*height/2.;
-        for (int i = 0; i < tableau.size(); ++i) {
-            std::vector<std::string> listTableau = split(tableau[i], delimiter);
+
+        /*for (int i = 0; i < vertices.size(); ++i) { //Pour déssiner les points
+            std::vector<std::string> listTableau = split(vertices[i], delimiter);
             float x = (atof(listTableau[0].c_str())+1.)*width/2.;
             float y = (atof(listTableau[1].c_str())+1.)*height/2.;
             image.set(x,y,white);
+        }*/
+
+        for (int i = 0; i < edges.size(); ++i) {
+
+            std::vector<std::string> listArc = split(edges[i], delimiter);
+
+            int point1 = (atoi(listArc[0].c_str()) - 1.);
+            int point2 = (atoi(listArc[1].c_str()) - 1.);
+            int point3 = (atoi(listArc[2].c_str()) - 1.);
+
+            std::vector<std::string> coordonnees1 = split(vertices[point1], delimiter);
+            float x1 = (atof(coordonnees1[0].c_str()) + 1.) * width / 2.;
+            float y1 = (atof(coordonnees1[1].c_str()) + 1.) * height / 2.;
+
+            std::vector<std::string> coordonnees2 = split(vertices[point2], delimiter);
+            float x2 = (atof(coordonnees2[0].c_str())+1.)*width/2.;
+            float y2 = (atof(coordonnees2[1].c_str())+1.)*height/2.;
+
+            std::vector<std::string> coordonnees3 = split(vertices[point3], delimiter);
+            float x3 = (atof(coordonnees3[0].c_str())+1.)*width/2.;
+            float y3 = (atof(coordonnees3[1].c_str())+1.)*height/2.;
+
+            line(x1, y1, x2, y2, image, white);
+            line(x1, y1, x3, y3, image, white);
+            line(x2, y2, x3, y3, image, white);
         }
     }
     else{
         std::cerr << "Erreur" << std::endl;
     }
-
-
-    /*std::vector<std::string> listElements;
-    std::vector<std::string> elements;
-    std::vector<std::string> vect = read('obj/african_head.obj', listElements());*/
-
-    /*for (int i = 0; i < vect.size(); i++) {
-
-            Vec3f v0 = model->vert(face[j]);
-            Vec3f v1 = model->vert(face[(j+1)%3]);
-            int x0 = (v0.x+1.)*width/2.;
-            int y0 = (v0.y+1.)*height/2.;
-            int x1 = (v1.x+1.)*width/2.;
-            int y1 = (v1.y+1.)*height/2.;
-            line(x0, y0, x1, y1, image, white);
-
-    }
-
-    char delimiter = ' ';*/
-
-
 
     image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
     image.write_tga_file("output.tga");
