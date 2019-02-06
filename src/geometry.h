006b8ad0d36fcbaa03e60f0fd62a0a8f4111bb70
cocosh03 <corentin.lexa@gmail.com>
2019-02-06 11:40:06 +0100

    normalisation

--- src/geometry.h
+++ src/geometry.h
@@ -12,8 +12,20 @@ struct Vecteur2D{
     float x, y;
 };
 
-struct Vecteur3D{
-    float x, y, z;
+template <typename T> class Vecteur3D{
+public :
+    Vecteur3D() : x(0),y(0),z(0){}
+    Vecteur3D(T xx , T yy, T zz) : x(xx),y(yy),z(zz){}
+    T x,y,z;
+
+    float norm(){
+        return std::sqrt(x*x+y*y+z*z);
+    }
+    Vecteur3D<T> & normalize(T l = 1){
+        *this = (*this)*(l/norm());
+        return *this;
+    }
 };
 
+typedef Vecteur3D<float> Vecteur3DF;
 #endif //RENDU3D_GEOMETRY_H
